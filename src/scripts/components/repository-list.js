e18adb3d26da2ddee501165cafde87630c5d2d60
Taneli Hartikainen <taneli.hartikainen89@gmail.com>
2015-09-13 15:26:40 +0300

    Filter contributions from owned repositories.

--- src/scripts/components/repository-list.js
+++ src/scripts/components/repository-list.js
@@ -1,9 +1,34 @@
 import element from 'virtual-element'
 
 export function render({ props }) {
+  let forked = props.repos.filter(repo => repo.fork)
+
   return (
     <section class="repository-list">
-      <ul>{props.repos.map(repo => <li>{repo.name}</li>)}</ul>
+      <h2>Repositories</h2>
+      <ul>
+        {props.repos.filter(repo => !repo.fork).map(renderOwnedRepository)}
+      </ul>
+      <h2>Contributions</h2>
+      <ul>
+        {props.repos.filter(repo => repo.fork).map(renderForkedRepository)}
+      </ul>
     </section>
+  )
+}
+
+function renderOwnedRepository(repo) {
+  return (
+    <li class="repository">
+      {repo.name}
+    </li>
+  )
+}
+
+function renderForkedRepository(repo) {
+  return (
+    <li class="repository fork">
+      {repo.name}
+    </li>
   )
 }
